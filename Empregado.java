573adf935a9074219a4025321dd415c633297b87
Arthur-cmd256 <arthur.nascimento2020@outlook.com>
2021-08-12 15:27:45 -0300

    Teste realizado com sucesso

--- Empregado.java
+++ Empregado.java
@@ -6,6 +6,25 @@ public class Empregado{
     private double comissao;
     private double bonus;
 
+    public void setSalario (double salario){
+        this.salario = salario;
+    }
+
+    public void setComissao (double comissao){
+        this.comissao = comissao;
+    }
+    public void setBonus (double bonus){
+        this.bonus = bonus;
+    }
+
+    public void setTipo (int tipo){
+        this.tipo = tipo;
+    }
+
+    public int getTipo(){
+        return this.tipo;
+    }
+
     public double calculaSalario(){
         if (tipo == 1 ){
             return salario;
